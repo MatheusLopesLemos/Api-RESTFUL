01ec0352ff9efdd8507d7688a0772557601d85fe
MatheusLopesLemos <m.lopeslemos96@gmail.com>
2024-10-01 16:28:33 -0300

    Adicionados Métodos Post e GetAll

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ const conn = require('./db/conn');
 
 conn();
 
+// Routes
+
+const routes = require('./routes/router');
+
+app.use('/api', routes);
+
 app.listen(3000, function(){
     console.log('Server is running on port 3000');
 });
